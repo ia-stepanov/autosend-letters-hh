57756fcb45a4e3c8ee9b73fad94560e980f195b3
Kurkuza <danilsherrbakov1993@gmail.com>
2023-08-28 17:56:00 +0300

    Added prettier

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 // Пауза
 function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 async function init() {
-  var vacancies = document.querySelectorAll('[data-qa="vacancy-serp__vacancy_response"]');
-  var vacancy = document.querySelector('[data-qa="vacancy-response-link-top"]');
-  var i = 0;
+  var vacancies = document.querySelectorAll(
+    '[data-qa="vacancy-serp__vacancy_response"]'
+  )
+  var vacancy = document.querySelector('[data-qa="vacancy-response-link-top"]')
+  var i = 0
 
   // Функция для автоматического выбора резюме
   function selectResume() {
-    var resume = document.querySelector('#resume_ID_РЕЗЮМЕ');
-    var message = document.querySelector('[data-qa="vacancy-response-letter-toggle"]');
+    var resume = document.querySelector('#resume_ID_РЕЗЮМЕ')
+    var message = document.querySelector(
+      '[data-qa="vacancy-response-letter-toggle"]'
+    )
 
     if (!message) {
-      resume.click();
+      resume.click()
     } else {
-      resume.click();
-      message.click();
+      resume.click()
+      message.click()
     }
   }
 
@@ -26,8 +30,8 @@ async function init() {
     // Шаблон Сопроводительного письма
     var vacancyTitle = document.querySelector(
       '.bloko-modal-header_outlined > div'
-    ).textContent;
-    var vacancyName = vacancyTitle.slice(1, vacancyTitle.length - 1);
+    ).textContent
+    var vacancyName = vacancyTitle.slice(1, vacancyTitle.length - 1)
     var messagesData = {
       frontend: `Добрый день! 
 
@@ -38,67 +42,69 @@ async function init() {
 Буду с нетерпением ждать ответа и возможности обсудить условия работы и взаимные ожидания на собеседовании. Спасибо, что уделили время. 
 
 Контактные данные прилагаю.`,
-    };
+    }
 
     var messageArea = document.querySelector(
       '[data-qa="vacancy-response-popup-form-letter-input"]'
-    );
-    messageArea.value = '';
-    messageArea.value = messagesData.frontend;
+    )
+    messageArea.value = ''
+    messageArea.value = messagesData.frontend
 
     // Добавить изменения в поле текста
-    var evt = document.createEvent('HTMLEvents');
-    evt.initEvent('change', true, true);
-    messageArea.dispatchEvent(evt);
+    var evt = document.createEvent('HTMLEvents')
+    evt.initEvent('change', true, true)
+    messageArea.dispatchEvent(evt)
 
     // Отправить отклик
-    var btnSubmit = document.querySelector('[data-qa="vacancy-response-submit-popup"]');
-    btnSubmit.click();
+    var btnSubmit = document.querySelector(
+      '[data-qa="vacancy-response-submit-popup"]'
+    )
+    btnSubmit.click()
   }
 
   // Вызвать функцию на странице с вакансией
   if (vacancy) {
-    vacancy.click();
+    vacancy.click()
 
-    await delay(1000);
-    selectResume();
+    await delay(1000)
+    selectResume()
 
-    await delay(500);
-    handlerCoverLetter();
+    await delay(500)
+    handlerCoverLetter()
   }
   // Иначе вызвать функцию на странице со списком вакансий
   else {
     while (i <= vacancies.length) {
-      vacancies[i].click();
+      vacancies[i].click()
 
-      await delay(1000);
-      selectResume();
+      await delay(1000)
+      selectResume()
 
-      await delay(500);
-      handlerCoverLetter();
-      i++;
+      await delay(500)
+      handlerCoverLetter()
+      i++
 
-      await delay(1000);
+      await delay(1000)
     }
   }
 }
 
 // Добавить на панель доп. функционал
-(async function addNavLinks() {
-  await delay(1000);
+;(async function addNavLinks() {
+  await delay(1000)
 
   const navLinks = document.querySelectorAll(
     '.supernova-navi-item.supernova-navi-item_lvl-2.supernova-navi-item_no-mobile'
-  );
+  )
 
-  const itemLetters = document.createElement('div');
+  const itemLetters = document.createElement('div')
 
   function createElement(item, attribute, title) {
     item.classList.add(
       'supernova-navi-item',
       'supernova-navi-item_lvl-2',
       'supernova-navi-item_no-mobile'
-    );
+    )
 
     item.innerHTML = `
     <a
@@ -109,11 +115,11 @@ async function init() {
       ${title}
     </a>
     <div class="supernova-navi-underline">${title}</div>
-    `;
+    `
   }
 
-  createElement(itemLetters, 'handler-letters', 'Отправить отклики');
+  createElement(itemLetters, 'handler-letters', 'Отправить отклики')
 
-  navLinks[2].append(itemLetters);
-  document.querySelector('[handler-letters]').addEventListener('click', init);
-})();
+  navLinks[2].append(itemLetters)
+  document.querySelector('[handler-letters]').addEventListener('click', init)
+})()
