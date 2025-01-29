6058e22fec80963a20d903e6b87c5aba42b051e8
IlKhr <khorishko.ilya@gmail.com>
2025-01-29 20:15:39 +0300

    feat(design): add work with design january 2025

--- script.js
+++ script.js
@@ -1,103 +1,154 @@
-// Пауза
-function delay(ms) {
+// Функция паузы
+var delay = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
-}
+};
 
-async function init() {
-  var vacancies = document.querySelectorAll('[data-qa="vacancy-serp__vacancy_response"]');
-  var vacancy = document.querySelector('[data-qa="vacancy-response-link-top"]');
-  var i = 0;
+// получить кнопку "откликнуться" после заполнения сопроводительного письма
+var getResponseSubmitButton = () =>
+  document.querySelector('[data-qa="vacancy-response-submit-popup"]');
 
-  // Функция для автоматического выбора резюме
-  function selectResume() {
-    var resume = document.querySelector('#resume_ID_РЕЗЮМЕ');
-    var message = document.querySelector('[data-qa="vacancy-response-letter-toggle"]');
+var resolveRelocationConfirmModal = () => {
+  var buttonConfirmRelocation = document.querySelector(
+    '[data-qa="relocation-warning-confirm"]'
+  );
 
-    if (!message) {
-      resume.click();
-    } else {
-      resume.click();
-      message.click();
-    }
+  if (!buttonConfirmRelocation) {
+    return;
   }
+  buttonConfirmRelocation.click();
+};
 
-  // Функция для автоматической отправки Сопроводительного письма
-  function handlerCoverLetter() {
-    // Шаблон Сопроводительного письма
-    var vacancyTitle = document.querySelector(
-      '.bloko-modal-header_outlined > div'
-    ).textContent;
-    var vacancyName = vacancyTitle.slice(1, vacancyTitle.length - 1);
-    var messagesData = {
-      frontend: `Добрый день! 
+var resolveResponseMessageModal = async () => {
+  var btnSubmit = getResponseSubmitButton();
 
-Меня заинтересовала предложенная Вами вакансия ${vacancyName}. Ознакомившись с перечнем требований к кандидатам, пришел к выводу, что мой опыт работы позволяют мне претендовать на данную должность. 
+  // если мы обнаружили кнопку отправки, то...
+  if (!btnSubmit) {
+    throw Error("Submit button not exists or renamed");
+  }
+  // выбираем резюме
+  selectResume();
+
+  await delay(1137);
+  // заполняем сопроводительное и отправляем его
+  await handlerCoverLetter();
+};
+
+var getMessage = (vacancyName) => `Добрый день!
+
+Меня заинтересовала предложенная Вами вакансия ${vacancyName}. Ознакомившись с перечнем требований к кандидатам, пришел к выводу, что мой опыт работы позволяют мне претендовать на данную должность.
 
 Обладаю высоким уровнем фронтенд-разработки, свободно говорю по-английски. В работе ответствен, пунктуален и коммуникабелен.
 
-Буду с нетерпением ждать ответа и возможности обсудить условия работы и взаимные ожидания на собеседовании. Спасибо, что уделили время. 
+Буду с нетерпением ждать ответа и возможности обсудить условия работы и взаимные ожидания на собеседовании. Спасибо, что уделили время.
 
-Контактные данные прилагаю.`,
-    };
+Контактные данные прилагаю.`;
 
-    var messageArea = document.querySelector(
-      '[data-qa="vacancy-response-popup-form-letter-input"]'
-    );
-    messageArea.value = '';
-    messageArea.value = messagesData.frontend;
+var resolveModal = async () => {
+  await delay(800);
 
-    // Добавить изменения в поле текста
-    var evt = document.createEvent('HTMLEvents');
-    evt.initEvent('change', true, true);
-    messageArea.dispatchEvent(evt);
+  // на случай если вылазит модальное окно о "вы живёте не там, готовы переехать?"
+  resolveRelocationConfirmModal();
 
-    // Отправить отклик
-    var btnSubmit = document.querySelector('[data-qa="vacancy-response-submit-popup"]');
-    btnSubmit.click();
+  await delay(765);
+
+  await resolveResponseMessageModal();
+
+  await delay(400);
+};
+
+var pasteMessageToArea = async (message) => {
+  var messageArea = document.querySelector(
+    '[data-qa="vacancy-response-popup-form-letter-input"]'
+  );
+
+  var nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+    window.HTMLTextAreaElement.prototype,
+    "value"
+  ).set;
+
+  nativeInputValueSetter.call(messageArea, message);
+
+  messageArea.dispatchEvent(new Event("input", { bubbles: true }));
+};
+
+// Функция для автоматического выбора резюме
+var selectResume = () => {
+  var resume = document.querySelector("#resume_ID_РЕЗЮМЕ");
+
+  var message = document.querySelector(
+    '[data-qa="vacancy-response-letter-toggle"]'
+  );
+
+  if (!message) {
+    resume.click();
+  } else {
+    resume.click();
+    message.click();
   }
+};
+
+// Функция для автоматической отправки Сопроводительного письма
+var handlerCoverLetter = async () => {
+  // Шаблон Сопроводительного письма
+  var vacancyTitle = document.querySelector(
+    ".bloko-modal-header_outlined > div"
+  ).textContent;
+  var vacancyName = vacancyTitle.slice(1, vacancyTitle.length - 1);
+
+  // Добавить изменения в поле текста
+  await pasteMessageToArea(getMessage(vacancyName));
+
+  // Отправить отклик
+  await delay(1192);
+
+  var btnSubmit = getResponseSubmitButton();
+  btnSubmit.click();
+};
+
+var init = async () => {
+  var vacancies = document.querySelectorAll(
+    '[data-qa="vacancy-serp__vacancy_response"]'
+  );
+  var vacancy = document.querySelector('[data-qa="vacancy-response-link-top"]');
 
   // Вызвать функцию на странице с вакансией
   if (vacancy) {
     vacancy.click();
 
-    await delay(1000);
+    await delay(1293);
     selectResume();
 
-    await delay(500);
-    handlerCoverLetter();
+    await delay(894);
+    await handlerCoverLetter();
   }
   // Иначе вызвать функцию на странице со списком вакансий
   else {
-    while (i <= vacancies.length) {
-      vacancies[i].click();
+    for (let i = 0; i < vacancies.length; i += 2) {
+      var vacancy = vacancies[i];
 
-      await delay(1000);
-      selectResume();
+      vacancy.click();
 
-      await delay(500);
-      handlerCoverLetter();
-      i++;
-
-      await delay(1000);
+      await delay(600);
+      await resolveModal();
     }
   }
-}
+};
 
 // Добавить на панель доп. функционал
 (async function addNavLinks() {
   await delay(1000);
 
-  const navLinks = document.querySelectorAll(
-    '.supernova-navi-item.supernova-navi-item_lvl-2.supernova-navi-item_no-mobile'
+  var navLinks = document.querySelectorAll(
+    ".supernova-navi.supernova-navi_lvl-2.HH-Supernova-NotificationManager-Container.supernova-navi_shaded.supernova-navi_dashboard-redesign"
   );
 
-  const itemLetters = document.createElement('div');
+  var itemLetters = document.createElement("div");
 
   function createElement(item, attribute, title) {
     item.classList.add(
-      'supernova-navi-item',
-      'supernova-navi-item_lvl-2',
-      'supernova-navi-item_no-mobile'
+      "supernova-navi-item",
+      "supernova-navi-item_lvl-2",
+      "supernova-navi-item_no-mobile"
     );
 
     item.innerHTML = `
@@ -112,8 +163,8 @@ async function init() {
     `;
   }
 
-  createElement(itemLetters, 'handler-letters', 'Отправить отклики');
+  createElement(itemLetters, "handler-letters", "Отправить отклики");
 
-  navLinks[2].append(itemLetters);
-  document.querySelector('[handler-letters]').addEventListener('click', init);
+  navLinks[0].append(itemLetters);
+  document.querySelector("[handler-letters]").addEventListener("click", init);
 })();
